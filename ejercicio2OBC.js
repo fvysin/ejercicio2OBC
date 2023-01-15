468476ce3b29ffd927a85bb2084f6f9e14ca7ca8
fvysin <118851333+fvysin@users.noreply.github.com>
2023-01-15 21:40:42 +0100

    Add files via upload

---
+++ ejercicio2OBC.js
@@ -0,0 +1,11 @@
+var nombre="florencia";
+var edad=29;
+var desarrollador=false;
+var fechaNacimiento= new Date(30/04/1993);
+var libroFavorito={
+    nombre:"harry potter",
+    autor:"Jk Rowling",
+    fecha:2002,
+    url:"www.harrypotter.com,"
+
+}
